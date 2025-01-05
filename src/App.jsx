5bdf19fe9fcdcde92b5be077ac2b6a34701fe457
Loua Ben Afia <loua12benafia@gmail.com>
2025-01-05 13:59:01 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter } from "react-router-dom";
+import {About , Contact , Experience , Feedbacks , Hero , Navbar , StarsCanvas} from './components'; //, Tech , Works 
 
 const App = () => {
   return (
-    <div> 3D Developer Portfolio</div>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <Navbar/>
+        <Hero/>
+      </div>
+      <About/>
+      <Contact/>
+      <Experience/>
+      <Feedbacks/>
+      <div className="relative z-0">
+        <Contact/>
+        <StarsCanvas/>
+      </div>
+
+      
+
+    </div>
+    </BrowserRouter>
   )
 }
 
